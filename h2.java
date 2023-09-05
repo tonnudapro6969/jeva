82364b7381aecef1c7ef7069919c3a33c7db9f9f
tonnudapro6969 <115454824+tonnudapro6969@users.noreply.github.com>
2023-09-05 15:25:07 +0300

    Add files via upload

---
+++ h2.java
@@ -0,0 +1,24 @@
+public class h2 {
+    public static void main(String[] args) {
+    int a = 10;
+	float b = 39.370f;
+	float jagamine;
+		
+        jagamine = a / b;
+		
+		System.out.println(jagamine + "  meetrit");
+	    number2();
+    }
+
+    static void number2(){
+	
+    
+    
+    
+    
+    System.out.println("Tere maailm!");    
+    }
+}
+
+
+
---
+++ muutujad.java
@@ -0,0 +1,17 @@
+
+public class muutujad {
+
+	public static void main(String[] args) {
+		String eesnimi = new String();
+		eesnimi = "tonis";
+        
+        String klass = new String();
+        klass = "IT-22";
+        int vanus = 3;
+        float pikkus = 2.9f;
+        
+        System.out.println(eesnimi +" on "+ vanus + " aastane , " + pikkus + "  pikk ja käib  " + klass);
+
+	}
+}
+
