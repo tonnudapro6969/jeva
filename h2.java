07a5a698c3ab6b1ce8ba26bf20d03d3d180f84a9
tonnudapro6969 <115454824+tonnudapro6969@users.noreply.github.com>
2023-09-07 13:49:17 +0300

    Add files via upload

--- h2.java
+++ h2.java
@@ -1,23 +1,39 @@
+
 public class h2 {
     public static void main(String[] args) {
-    int a = 10;
-	float b = 39.370f;
-	float jagamine;
+        int a = 10;
+	    float b = 39.370f;
+	    float jagamine;
 		
         jagamine = a / b;
 		
 		System.out.println(jagamine + "  meetrit");
 	    number2();
+        number3();
     }
 
     static void number2(){
-	
-    
-    
-    
-    
-    System.out.println("Tere maailm!");    
+   
+        int c = 6;
+        int d = 3;
+        float pi = 3.14f;
+        
+        float arv = c*d*pi;
+        System.out.println( arv + "HJAJHAHAJA" );    
     }
+    static void number3(){
+        
+        int minart = 300;
+        long hours = ( minart /60);
+        System.out.printf(hours + "  tund?" );
+
+    }
+
+
+
+
+
+
 }
 
 
