9d12a20d6312bf8edf6eec46f33845a887d145bd
tonnudapro6969 <115454824+tonnudapro6969@users.noreply.github.com>
2023-09-12 13:47:52 +0300

    Add files via upload

---
+++ ylesanne4.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class ylesanne4{
+    
+	public static void main(String[] args) {
+        
+        Scanner penis = new Scanner(System.in); 	//kasutaja sisestus
+		System.out.print("Sisesta mind x lause palun :) : "); 
+		
+        System.out.println("Teie kirjutasite: "penis.toUpperCase());
+        
+
+
+
+
+
+
+
+
+    }
+}
