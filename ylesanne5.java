7c4c25aab2417d96d86117133deb6003bad30703
tonnudapro6969 <115454824+tonnudapro6969@users.noreply.github.com>
2023-09-14 09:13:37 +0300

    Add files via upload

---
+++ ylesanne5.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ylesanne5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        for (int i = 0; i < 3; i++) {
+            System.out.println("sisesta eisemne arv: ");
+            double esimeneArv = scanner.nextDouble();
+            
+            System.out.println("sisesta jagaja: ");
+            double teineArv = scanner.nextDouble();
+            
+            if (esimeneArv >= 0 && teineArv > 0) {
+                double tulemus = esimeneArv / teineArv;
+                System.out.println("vastus: " + tulemus);
+            } else {
+                System.out.println("Midagi on valesti. Soovitan vetsus enesetapp sooritada")
+        }
+        
+        scanner.close();
+    }
+}
