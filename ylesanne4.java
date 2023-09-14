de9ce361e7624005d9200e3fabe1b7eb7871b8c0
tonnudapro6969 <115454824+tonnudapro6969@users.noreply.github.com>
2023-09-14 08:58:40 +0300

    Add files via upload

--- ylesanne4.java
+++ ylesanne4.java
@@ -3,18 +3,32 @@ public class ylesanne4{
     
 	public static void main(String[] args) {
         
-        Scanner penis = new Scanner(System.in); 	//kasutaja sisestus
-		System.out.print("Sisesta mind x lause palun :) : "); 
-		
-        System.out.println("Teie kirjutasite: "penis.toUpperCase());
-        
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("oghfeohe fknklef ko suvaline teks x3: ");
+        String kasutajaTekst = scanner.nextLine();
 
+        String suuredTähed = kasutajaTekst.toUpperCase();
+        System.out.println("Suur täaeht: " + suuredTähed);
 
+        int märkideArv = kasutajaTekst.length();
+        System.out.println("markide arv: " + märkideArv);
+        String[] sõnad = kasutajaTekst.split("\\s+");
+        int sõnadeArv = sõnad.length;
+        System.out.println("sünade arv: " + sõnadeArv);
+        String esimeneSõna = sõnad[0];
+        System.out.printf("ESIEME SONA: %s%n", esimeneSõna);
 
-
-
-
-
+        scanner.close();
+   
+   
     }
 }
+
+
+
+
+
+
+
+    
