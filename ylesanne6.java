bfe64557430fb8997ecfb303fa2f7f7b8d687565
tonnudapro6969 <115454824+tonnudapro6969@users.noreply.github.com>
2023-09-19 11:36:06 +0300

    Add files via upload

--- ylesanne6.java
+++ ylesanne6.java
@@ -1,13 +1,76 @@
 
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner; 
 public class ylesanne6 {
+// tonis kandma it.-222 jahrjusuts 6
+
 	public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in); //System.in is a standard input stream  
+        
+		Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter integers (leave blank to finish):");
+        
+        int sum = 0;
+        int count = 0;
+
+        while (true) {
+            System.out.print("Enter an integer: ");
+            String input = scanner.nextLine().trim(); // Remove leading/trailing whitespace
+
+            if (input.isEmpty()) {
+                break; // Exit the loop if the input is empty
+            }
+
+            try {
+                int number = Integer.parseInt(input);
+                sum += number;
+                count++;
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input! Please enter an integer.");
+            }
+        }
+
+        if (count > 0) {
+            double average = (double) sum / count;
+            System.out.println("Sum of entered integers: " + sum);
+            System.out.println("Average of entered integers: " + average);
+        } else {
+            System.out.println("No valid integers entered.");
+        }
+
+        scanner.close();
+    }
+
+
+		
+		
+		
+		Scanner sc= new Scanner(System.in); 
         System.out.print("Enter a string: ");  
-        String str= sc.nextLine();              //reads string   
+        String str= sc.nextLine();              
         System.out.print("You have entered: "+str);             
-        }  
-        }  
+        
+
+
+//N HNYJ PÖ88P88888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888ÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄ
+//ÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄ
+        File minuFail = new File("MinuTekstifail.txt");
+        try {
+			BufferedWriter bw = new BufferedWriter(new FileWriter(minuFail));
+			bw.write(str);
+			bw.close();
+			System.out.println("Fail loodud!");
+		} catch (IOException e) {
+			e.printStackTrace();
+		
+		
+		
+
+ }  
 
 
 
