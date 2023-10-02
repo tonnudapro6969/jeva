21409fad00f4e34393936af6c8353a3bea029887
tonnudapro6969 <115454824+tonnudapro6969@users.noreply.github.com>
2023-10-02 11:16:42 +0300

    Add files via upload

---
+++ ylesanne82.java
@@ -0,0 +1,25 @@
+
+ 
+public class ylesanne82 {
+    public static void main(String[] args) {
+   
+    String nimi;
+    int vanus;
+    
+    //konstruktor
+    public void lehm(String n, int v) {
+    this.nimi = n;
+    this.vanus = v;
+    }
+    
+    public void lehm(){
+    this.nimi = "<lehm>";
+    this.vanus = 0;
+    }
+    
+    //isendimeetodid
+    public void tutvustaEnd(){
+    System.out.println("Mina olen "+nimi+", "+vanus+"! yfhjfghfghf!");
+        }
+   }
+}
