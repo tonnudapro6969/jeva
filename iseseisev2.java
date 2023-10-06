013d197e8210e3c9d648d8798553c7e8e18ee1cb
tonnudapro6969 <115454824+tonnudapro6969@users.noreply.github.com>
2023-10-06 10:17:29 +0300

    Add files via upload

--- iseseisev2.java
+++ iseseisev2.java
@@ -2,18 +2,21 @@
 
 import java.util.Random;
 
-
 public class iseseisev2 {
  
-    public static void main(String[] args) {
+    public static void arvutus(){
         Random sitt = new Random(); //randomi generatsiooni see kood 
 
-for(int i =0; i < 1; i++) //phm loop
-{
+    for(int i =0; i < 1; i++) //phm loop
+    {
     int aus = sitt.nextInt(6) + 0;  //genereerib x arvu 0-6 ma panin +0 sest siis on ilusti vahemikus ja saab arv olla ka 0
-    System.out.println(aus);  //print
-}
 
+    }
+}
+    return aus
+    public static void main(String[] args) {
+    
+    System.out.println(aus);  //print
 
     }
 }
