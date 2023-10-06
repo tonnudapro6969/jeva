b0cadcbe77bbc8e61ce5e9a4804be081a7d849d3
tonnudapro6969 <115454824+tonnudapro6969@users.noreply.github.com>
2023-10-06 10:08:31 +0300

    Add files via upload

--- iseseisev20.java
+++ iseseisev20.java
@@ -1,16 +1,27 @@
 import java.util.Scanner;
 
 public class iseseisev20 {
-    public static void main(String[] args) {
+    public static arvutused(){
         Scanner scanner = new Scanner(System.in); //phm loob scanneri ehk input commandi
 
         System.out.print("sisesta  arv 1 ja 0 : ");  //kusib kasutajalt input
-        String binarv = scanner.nextLine();   //viib kasutaja sisestatud arvu edasi? et saaks teistes commandides kasutada?
-
-        int decarv = Integer.parseInt(binarv, 2);   //teisendab binaararvu decimal arvuks
-        System.out.println("decimal susteemi arv on: " + decarv); //prindib vastuse valja
         scanner.close();
+
+        try {
+            String binarv = scanner.nextLine();   //viib kasutaja sisestatud arvu edasi? et saaks teistes commandides kasutada?
+            int decarv = Integer.parseInt(binarv, 2);   //teisendab binaararvu decimal arvuks
+        }
+        
+        return decarv;
     }
-public void close();
+       
+    }
+    public static void main(String[] args) {
+        
+        int decarv = arvutused(decarv)
+
+        System.out.println("decimal susteemi arv on: " + decarv); //prindib vastuse valja
+
+
 
 }
