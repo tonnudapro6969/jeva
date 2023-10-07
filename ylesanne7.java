a4e239d6915ea3342f77982545a17321abd7a310
tonnudapro6969 <115454824+tonnudapro6969@users.noreply.github.com>
2023-10-07 17:38:39 +0300

    Add files via upload

--- ylesanne7.java
+++ ylesanne7.java
@@ -1,25 +1,27 @@
+
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ylesanne7 {
     public static void main(String[] args) {
-        System.out.println("ANAALSEKSSESKESEKSE YLESANNE 7");
-        
         Scanner scanner = new Scanner(System.in);
+        boolean tekst = true;  //boolean = tekts ehk kui seal all on tekst = false siis prindib tektsi
+            try {
+                System.out.print("Esiemen arv: "); //kusib esimest arvu
+                int arv1 = scanner.nextInt();   //salvestab antud arvu arv1 
 
-        System.out.print("esimene arv kallis:  ");
-        double A = scanner.nextDouble();
+                System.out.print("teine arv: ");  //kusib teise arvu
+                int arv2 = scanner.nextInt(); //salvestab antud arvu arv2
 
-        System.out.print("B ARV EH KTEINE PALUN: ");
-        double B = scanner.nextDouble();
+                int summa = arv1 + arv2; //arvutab ja prindib tulemuse
+                System.out.println("vastus: " + summa);  //prindib
 
-        if (A == B) {
-            System.out.println("UAAAAAAAAAAAAAAHHHHHHHHHHHHHHHHHH.");
-            System.out.print("number palun pissikaka: ");
-            B = scanner.nextDouble();
-        }
-
-        double jagamine = A / B;
-        System.out.println( "Vastus on :  " +  jagamine );
+                tekst = false;    //ülemine programm sulgeb ehk vastust ei prindita välja
+            } catch (InputMismatchException e) {    //näiutab et kasutaja sisestas tähe mitte arvu
+                System.out.println("ära sisesta teksti, aInult arvud!"); //  prindib välja errori et sisestasid teksti mi9tte arvud
+            }
         scanner.close();
+    }
+}
 
-}   }
+
